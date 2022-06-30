1e1e784d0ba71572de9c4cf57087e5ef01e4515d
Mavis <tranmingtan4869@gmail.com>
2022-06-30 19:37:11 +0700

    Added 30-06-2022b

--- src/splashscreen/splashscreen.js
+++ src/splashscreen/splashscreen.js
@@ -1,38 +1,113 @@
 /* eslint-disable prettier/prettier */
-import { StyleSheet, Text, View, TouchableOpacity, TextInput, Dimensions, SafeAreaView, ImageBackground, Image, StatusBar } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, Dimensions, SafeAreaView, ImageBackground, Image, StatusBar, Animated } from 'react-native';
 import React, { Component } from 'react'
+import { useState, useEffect, useRef } from 'react';
+
 
 const widthWindow = Dimensions.get('window').width;
 const heightWindow = Dimensions.get('window').height;
 
 function Splashscreen({navigation}) {
+    const LeftMotion = useRef(new Animated.Value(-1000)).current;
+    const TopMotion = useRef(new Animated.Value(0.2* heightWindow)).current;
+    const SizeMotionLogo = useRef(new Animated.Value(100)).current;
+    const FontWeight = useRef(new Animated.Value(100)).current;
+
+    useEffect(() => {
+        Animated.sequence([
+            Animated.parallel([
+                Animated.timing(
+                    LeftMotion,
+                    {
+                        toValue: 200,
+                        duration: 3000,
+                        useNativeDriver: false,
+                    }),
+
+                    Animated.timing(
+                        SizeMotionLogo,
+                        {
+                            toValue: 150,
+                            duration: 3000,
+                            useNativeDriver: false,
+                        }),
+                    
+            ]),
+            
+            Animated.parallel([
+                Animated.timing(
+                    FontWeight,
+                    {
+                        toValue: 30,
+                        duration: 3000,
+                        useNativeDriver: false,
+                    }),
+                Animated.timing(
+                    LeftMotion,
+                    {
+                        toValue: 50,
+                        duration: 3000,
+                        useNativeDriver: false,
+                    }),
+                    
+                    Animated.timing(
+                        SizeMotionLogo,
+                        {
+                            toValue: 120,
+                            duration: 3000,
+                            useNativeDriver: false,
+                        }),
+            ]),
+
+            Animated.parallel([
+               
+                Animated.timing(
+                    TopMotion,
+                    {
+                        toValue: -500,
+                        duration: 1500,
+                        useNativeDriver: false,
+                    }),
+                    
+                    
+            ])
+
+        ]).start();
+        
+    }, [LeftMotion,FontWeight, SizeMotionLogo, TopMotion])
  
     setTimeout(() => {
         navigation.navigate('InTroduce');
-    }, 3000);
+    }, 8000);
     
     return (
-       <ImageBackground style={{width:'100%', height:'100%'}} source={require('../../drawble/Image/background_orange.jpg')}>
+       <SafeAreaView style={{width:'100%', height:'100%'}}>
            
                <StatusBar
                 hidden={true}
                />
-                <View style={{alignItems:'center', width: widthWindow, height: heightWindow, marginTop: 0.2* heightWindow }}>
-                    <Image style={{  }} source={require('../../drawble/Image/logonofill.png')}/>
-                    <Text style={{fontSize:35, fontWeight: 'bold', color: 'white', marginTop: 10}}>
-                        CAO ĐẲNG VIỄN ĐÔNG
-                    </Text>
-                    <View style={{flexDirection:'row', width:'80%', marginTop:10, justifyContent:'center', alignItems:'center'}}>
-                        <Text style={{fontSize:10, fontStyle:'italic', backgroundColor:'#FF261F', color:'white', padding: 5}}>
-                            CÓ VIỆC LÀM ĐÚNG NGÀNH - GIÀNH ĐẠI HỌC TOP TRÊN
-                        </Text>
-                        <Text style={{fontSize:10, fontStyle:'italic', backgroundColor:'#FFF000', color:'black', padding: 5}}>
-                            TPHCM
-                        </Text>
-                    </View>
-                </View>
-          
-       </ImageBackground>
+               <View style={{width: widthWindow, height: heightWindow, position:'relative'}}>
+                <Image style={{position:'absolute', top:-300, left:-300}} source={require('../../drawble/drawblev24/Path1.png')}/>
+                <Image style={{position:'absolute', bottom: -200, right:-100}} source={require('../../drawble/drawblev24/Path2.png')}/>
+                <Image style={{position:'absolute', bottom:-100, right:-250}} source={require('../../drawble/drawblev24/Path4.png')}/>
+
+                <Image style={{position:'absolute', bottom:-400, right:-100, width:widthWindow*2}} source={require('../../drawble/drawblev24/Path3.png')}/>
+
+                <Animated.View style={{width: widthWindow, height: heightWindow, marginTop: TopMotion }}>
+                            <View style={{width:'100%', alignItems:'center'}}>
+                                <Animated.Image style={{ width:SizeMotionLogo, height: SizeMotionLogo,  }} source={require('../../drawble/drawblev24/MaskGroup1.png')}/>
+                            </View>
+                            <Animated.View style={{marginLeft: LeftMotion, width:'500%'}}>
+                                <Animated.Text style={{fontSize:FontWeight, fontWeight: 'bold', color: 'white', marginTop: 20}}>
+                                    CAO ĐẲNG VIỄN ĐÔNG
+                                </Animated.Text>
+                            </Animated.View>
+                        
+                    </Animated.View>
+               </View>
+               
+                
+       </SafeAreaView>
     );
 }
 
--- src/splashscreen/splashscreenlogin.js
+++ src/splashscreen/splashscreenlogin.js
@@ -1,37 +1,108 @@
 /* eslint-disable prettier/prettier */
-import { StyleSheet, Text, View, TouchableOpacity, TextInput, Dimensions, SafeAreaView, ImageBackground, Image, StatusBar } from 'react-native';
-import React, { Component } from 'react'
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, Dimensions, SafeAreaView, ImageBackground, Image, StatusBar, Animated } from 'react-native';
+import React, { Component } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 const widthWindow = Dimensions.get('window').width;
 const heightWindow = Dimensions.get('window').height;
 
 function SplashscreenLogin({navigation}) {
  
+    const LeftMotion = useRef(new Animated.Value(-1000)).current;
+    const TopMotion = useRef(new Animated.Value(0.2* heightWindow)).current;
+    const SizeMotionLogo = useRef(new Animated.Value(100)).current;
+    const FontWeight = useRef(new Animated.Value(100)).current;
+
+    useEffect(() => {
+        Animated.sequence([
+            Animated.parallel([
+                Animated.timing(
+                    LeftMotion,
+                    {
+                        toValue: 200,
+                        duration: 1500,
+                        useNativeDriver: false,
+                    }),
+
+                    Animated.timing(
+                        SizeMotionLogo,
+                        {
+                            toValue: 150,
+                            duration: 1500,
+                            useNativeDriver: false,
+                        }),
+                    
+            ]),
+            
+            Animated.parallel([
+                Animated.timing(
+                    FontWeight,
+                    {
+                        toValue: 30,
+                        duration: 1500,
+                        useNativeDriver: false,
+                    }),
+                Animated.timing(
+                    LeftMotion,
+                    {
+                        toValue: 50,
+                        duration: 1500,
+                        useNativeDriver: false,
+                    }),
+                    
+                    Animated.timing(
+                        SizeMotionLogo,
+                        {
+                            toValue: 120,
+                            duration: 1500,
+                            useNativeDriver: false,
+                        }),
+            ]),
+
+            Animated.parallel([
+               
+                Animated.timing(
+                    TopMotion,
+                    {
+                        toValue: -500,
+                        duration: 1000,
+                        useNativeDriver: false,
+                    }),
+                    
+                    
+            ])
+
+        ]).start();
+        
+    }, [LeftMotion,FontWeight, SizeMotionLogo, TopMotion])
     setTimeout(() => {
         navigation.navigate('ControlTab');
-    }, 2000);
+    }, 4000);
     
     return (
-       <ImageBackground style={{width:'100%', height:'100%'}} source={require('../../drawble/Image/background_orange.jpg')}>
+        <ImageBackground style={{width:'100%', height:'100%'}} source={require('../../drawble/Image/background_orange.jpg')}>
            
                <StatusBar
                 hidden={true}
                />
-                <View style={{alignItems:'center', width: widthWindow, height: heightWindow, marginTop: 0.2* heightWindow }}>
-                    <Image style={{  }} source={require('../../drawble/Image/logonofill.png')}/>
-                    <Text style={{fontSize:35, fontWeight: 'bold', color: 'white', marginTop: 10}}>
-                        CAO ĐẲNG VIỄN ĐÔNG
-                    </Text>
-                    <View style={{flexDirection:'row', width:'80%', marginTop:10, justifyContent:'center', alignItems:'center'}}>
+                <Animated.View style={{width: widthWindow, height: heightWindow, marginTop: TopMotion }}>
+                    <View style={{width:'100%', alignItems:'center'}}>
+                        <Animated.Image style={{ width:SizeMotionLogo, height: SizeMotionLogo,  }} source={require('../../drawble/drawblev24/MaskGroup5.png')}/>
+                    </View>
+                    <Animated.View style={{marginLeft: LeftMotion, width:'500%'}}>
+                        <Animated.Text style={{fontSize:FontWeight, fontWeight: 'bold', color: 'white', marginTop: 20}}>
+                            CAO ĐẲNG VIỄN ĐÔNG
+                        </Animated.Text>
+                    </Animated.View>
+                    {/* <View style={{flexDirection:'row', width:'80%', marginTop:10, justifyContent:'center', alignItems:'center'}}>
                         <Text style={{fontSize:10, fontStyle:'italic', backgroundColor:'#FF261F', color:'white', padding: 5}}>
                             CÓ VIỆC LÀM ĐÚNG NGÀNH - GIÀNH ĐẠI HỌC TOP TRÊN
                         </Text>
                         <Text style={{fontSize:10, fontStyle:'italic', backgroundColor:'#FFF000', color:'black', padding: 5}}>
                             TPHCM
                         </Text>
-                    </View>
-                </View>
-          
+                    </View> */}
+                </Animated.View>
        </ImageBackground>
     );
 }
